27039a491fd3cc2c0b5934fcc9129f4261219971
juju4 <juju4@users.noreply.github.com>
2016-10-16 19:56:06 -0400

    sync repositories - minor lint, serverspec test with other value

--- test/integration/default/serverspec/sysctl_spec.rb
+++ test/integration/default/serverspec/sysctl_spec.rb
@@ -4,7 +4,8 @@ require 'serverspec'
 set :backend, :exec
 
 describe command('sysctl -p') do
-  its(:stdout) { should match /net.ipv4.icmp_echo_ignore_broadcasts = 1/ }
+#  its(:stdout) { should match /net.ipv4.icmp_echo_ignore_broadcasts = 1/ }
+  its(:stdout) { should match /net.ipv4.conf.default.log_martians = 1/ }
   its(:stdout) { should_not match /error: / }
   its(:exit_status) { should eq 0 }
 end
